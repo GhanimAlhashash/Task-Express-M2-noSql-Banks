7840ad9704979c0f7a5fe82af3ef8533d2867bed
Ghanimalhashash <Ghanim-alhashash@hotmail.com>
2024-11-12 10:32:04 +0300

    accountmongoos

--- api/accounts/accounts.controllers.js
+++ api/accounts/accounts.controllers.js
@@ -1,31 +1,77 @@
-let accounts = require('../../accounts');
+let accounts = require("../../accounts");
+const Account = require("../../models/Account");
 
-exports.accountCreate = (req, res) => {
-  const id = accounts[accounts.length - 1].id + 1;
-  const newAccount = { ...req.body, funds: 0, id };
-  accounts.push(newAccount);
-  res.status(201).json(newAccount);
-};
-
-exports.accountDelete = (req, res) => {
-  const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    accounts = accounts.filter((account) => account.id !== +accountId);
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+exports.accountsList = async (req, res) => {
+  try {
+    const accounts = await Account.find();
+    res.json(accounts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
-exports.accountUpdate = (req, res) => {
+exports.accountCreate = async (req, res) => {
+  // const id = accounts[accounts.length - 1].id + 1;
+  // const newAccount = { ...req.body, funds: 0, id };
+  // accounts.push(newAccount);
+  // res.status(201).json(newAccount);
+  try {
+    const newAccount = await Account.create(req.body);
+    res.status(201).json(newAccount);
+  } catch {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.accountDelete = async (req, res) => {
+  // const { accountId } = req.params;
+  // const foundAccount = accounts.find((account) => account.id === +accountId);
+  // if (foundAccount) {
+  //   accounts = accounts.filter((account) => account.id !== +accountId);
+  //   res.status(204).end();
+  // } else {
+  //   res.status(404).json({ message: "Account not found" });
+  // }
   const { accountId } = req.params;
-  const foundAccount = accounts.find((account) => account.id === +accountId);
-  if (foundAccount) {
-    foundAccount.funds = req.body.funds;
-    res.status(204).end();
-  } else {
-    res.status(404).json({ message: 'Account not found' });
+  try {
+    const foundAccount = await Account.findById(accountId);
+    if (!foundAccount) res.status(404).json({ message: "account not found" });
+
+    await foundAccount.deleteOne();
+
+    res.status(200).json({ message: "account deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.accountUpdate = async (req, res) => {
+  //   const {accountId} = req.params;
+  //   try {
+  // const foundAccount = await Account.findById(accountId);
+  // if (!foundAccount) res.status(404).json({message: "account not found"});
+
+  // const {username, funds} = req.body;
+
+  // foundAccount.username = username;
+  // foundAccount.funds = funds;
+  // await foundAccount.save()
+  //   } catch (error) {
+  //     res.status(500).json(foundAccount);
+
+  //   }
+  //   res.status(200).json(foundAccount)
+  try {
+    const { accountId } = req.params;
+    const foundAccount = await Account.findById(accountId);
+    if (foundAccount) {
+      await foundAccount.updateOne(req.body);
+      res.status(204).end();
+    } else {
+      res.status(404).json({ message: error.message });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
 
@@ -38,7 +84,7 @@ exports.getAccountByUsername = (req, res) => {
   const foundAccount = accounts.find(
     (account) => account.username === username
   );
-  if (req.query.currency === 'usd') {
+  if (req.query.currency === "usd") {
     const accountInUsd = { ...foundAccount, funds: foundAccount.funds * 3.31 };
     res.status(201).json(accountInUsd);
   }
---
+++ models/Account.js
@@ -0,0 +1,6 @@
+const { model, Schema } = require("mongoose");
+const AccountSchema = new Schema({
+  username: { type: String },
+  funds: { type: Number },
+});
+module.exports = model("Account", AccountSchema);
